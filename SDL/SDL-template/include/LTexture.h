ab3d6816e9510685501b9222ddb2c3d36ac8ef88
Mangrover007 <Mangrover@proton.me>
2026-08-26 17:46:25 +0530

    made a template because im tired of writing the same thing 500000 times

---
+++ SDL/SDL-template/include/LTexture.h
@@ -0,0 +1,32 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <string>
+
+extern SDL_Renderer* gRenderer;
+extern TTF_Font* gFont;
+
+class LTexture
+{
+public:
+    LTexture();
+
+    void render(int posX, int posY);
+
+    // Load image into a SDL_Texture
+    bool loadFromFile(const std::string& path);
+
+    // Load text into a SDL_Texture
+    bool loadTTF(const std::string& text, SDL_Color color);
+
+    void free();
+
+    int getWidth();
+    int getHeight();
+
+private:
+    SDL_Texture* mTexture;
+    int mHeight;
+    int mWidth;
+};
+
