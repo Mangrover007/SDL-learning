07d0cf0ad20e7d234f896b12589276a0f4df6680
Mangrover007 <Mangrover@proton.me>
2026-08-27 14:51:56 +0530

    how to calculate fps 100 (not 101 cuz precision is tanking)

---
+++ SDL/SDL-framerate/src/main.cpp
@@ -0,0 +1,170 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+
+
+#include <stdio.h>
+
+
+#include "../include/LButton.h"
+#include "../include/LTimer.h"
+
+
+// Core global variables
+SDL_Window* gWindow;
+SDL_Renderer* gRenderer;
+TTF_Font* gFont;
+
+
+#include "../include/globals.h"
+
+
+const char* fontPath = "/usr/share/fonts/OTF/ipam.ttf";
+const char* WINDOW_NAME = "HELLO template :D";
+
+
+bool init()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+	printf("Error initializing SDL: %s\n", SDL_GetError());
+	return false;
+    }
+
+    int IMG_INIT_FLAGS = IMG_INIT_PNG | IMG_INIT_JPG;
+    if (IMG_Init(IMG_INIT_FLAGS) != IMG_INIT_FLAGS)
+    {
+	printf("Error initializing SDL_image: %s\n", SDL_GetError());
+	return false;
+    }
+
+    if (TTF_Init() != 0)
+    {
+	printf("Error initializing SDL_ttf: %s\n", SDL_GetError());
+	return false;
+    }
+
+    gWindow = SDL_CreateWindow(
+		WINDOW_NAME,
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		SCREEN_WIDTH,
+		SCREEN_HEIGHT,
+		SDL_WINDOW_SHOWN
+	    );
+
+    if (gWindow == nullptr)
+    {
+	printf("Error creating SDL window: %s\n", SDL_GetError());
+	return false;
+    }
+
+    gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    if (gRenderer == nullptr)
+    {
+	printf("Error creating Renderer for SDL Window: %s\n", SDL_GetError());
+	return false;
+    }
+
+    gFont = TTF_OpenFont(fontPath, 28);
+
+    if (gFont == nullptr)
+    {
+	printf("Failed to load font: %s\n", TTF_GetError());
+	return false;
+    }
+
+    return true;
+}
+
+
+void close()
+{
+    SDL_DestroyWindow(gWindow);
+    SDL_DestroyRenderer(gRenderer);
+
+    TTF_CloseFont(gFont);
+    
+    TTF_Quit();
+    IMG_Quit();
+    SDL_Quit();
+}
+
+
+void handleEvent(SDL_Event& e)
+{
+    // write custom events handlers here   
+}
+
+
+const SDL_Color SDL_COLOR_BLACK = SDL_Color{0x00, 0x00, 0x00, 0xFF};
+
+#include <sstream>
+
+void mainLoop()
+{
+    bool quit = false;
+    SDL_Event e;
+
+    LTimer fpsTimer = LTimer();
+    int framesCounted = 0;
+
+    int fps = 0;
+
+    fpsTimer.start();
+
+    std::stringstream fpsText;
+    LTexture fpsTextTexture = LTexture();
+
+    while (quit == false)
+    {
+	while (SDL_PollEvent(&e) != 0)
+	{
+	    if (e.type == SDL_QUIT)
+	    {
+		quit = true;
+	    }
+	    else
+	    {
+		handleEvent(e);
+	    }
+	}
+
+	SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+	SDL_RenderClear(gRenderer);
+
+	// write code here
+	fps = framesCounted / (fpsTimer.getTicks() / 1000.f);
+
+	fpsText.str("");
+	fpsText << "FPS: " << fps;
+	fpsTextTexture.loadTTF(fpsText.str().c_str(), SDL_COLOR_BLACK);
+	fpsTextTexture.render((SCREEN_WIDTH - fpsTextTexture.getWidth()) / 2, (SCREEN_HEIGHT - fpsTextTexture.getHeight()) / 2);
+
+	SDL_RenderPresent(gRenderer);
+
+	framesCounted++;
+    }
+
+    printf("Exiting main loop...\n");
+}
+
+int main(int argc, char** argv)
+{
+    if (!init())
+    {
+	printf("Could not initialize the required libraries!\n");
+	return -1;
+    }
+
+    printf("Successfully initialized\n");
+    printf("Starting main loop...\n");
+
+    mainLoop();
+
+    close();
+
+    return 0;
+}
+
